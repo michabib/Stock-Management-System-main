1ec3fa10329ce3a8f20b8a512cdfe086563d6442
Princesse <michelinedjagou63@gmail.com>
2025-04-07 08:49:17 +0100

    push

--- pages/employee.php
+++ pages/employee.php
@@ -1,6 +1,33 @@
+<?php
+include'../includes/connection.php';
+include'../includes/sidebar.php';
+?>
+
+<?php 
+$query = 'SELECT ID, t.TYPE
+          FROM users u
+          JOIN type t ON t.TYPE_ID=u.TYPE_ID WHERE ID = '.$_SESSION['MEMBER_ID'].'';
+$result = mysqli_query($db, $query) or die (mysqli_error($db));
+
+while ($row = mysqli_fetch_assoc($result)) {
+  $Aa = $row['TYPE'];
+
+  if ($Aa=='User'){
+?>    
+    <script type="text/javascript">
+      // l'utilisateur sera redirigé
+      alert("Page restreinte ! Vous allez être redirigé vers le point de vente (POS)");
+      window.location = "pos.php";
+    </script>
+<?php 
+  }
+}   
+?>
+
 <div class="card shadow mb-4">
   <div class="card-header py-3">
-    <h4 class="m-2 font-weight-bold text-primary">Employé&nbsp;
+    <h4 class="m-2 font-weight-bold text-primary">
+      Employé&nbsp;
       <a href="#" data-toggle="modal" data-target="#employeeModal" type="button" class="btn btn-primary bg-gradient-primary" style="border-radius: 0px;">
         <i class="fas fa-fw fa-plus"></i>
       </a>
@@ -19,34 +46,46 @@
           </tr>
         </thead>
         <tbody>
-          <?php                  
-            $query = 'SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, j.JOB_TITLE FROM employee e JOIN job j ON e.JOB_ID=j.JOB_ID';
-            $result = mysqli_query($db, $query) or die (mysqli_error($db));
-            while ($row = mysqli_fetch_assoc($result)) {
-              echo '<tr>';
-              echo '<td>'. $row['FIRST_NAME'].'</td>';
-              echo '<td>'. $row['LAST_NAME'].'</td>';
-              echo '<td>'. $row['JOB_TITLE'].'</td>';
+<?php                  
+  $query = 'SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, j.JOB_TITLE 
+            FROM employee e 
+            JOIN job j ON e.JOB_ID=j.JOB_ID';
+  $result = mysqli_query($db, $query) or die (mysqli_error($db));
 
-              echo '<td align="right"> <div class="btn-group">
-                      <a type="button" class="btn btn-primary bg-gradient-primary" href="emp_searchfrm.php?action=edit & id='.$row['EMPLOYEE_ID'] . '"><i class="fas fa-fw fa-list-alt"></i> Détails</a>
-                    <div class="btn-group">
-                      <a type="button" class="btn btn-primary bg-gradient-primary dropdown no-arrow" data-toggle="dropdown" style="color:white;">
-                        ... <span class="caret"></span></a>
-                      <ul class="dropdown-menu text-center" role="menu">
-                        <li>
-                          <a type="button" class="btn btn-warning bg-gradient-warning btn-block" style="border-radius: 0px;" href="emp_edit.php?action=edit & id='.$row['EMPLOYEE_ID']. '">
-                            <i class="fas fa-fw fa-edit"></i> Modifier
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div> </td>';
-              echo '</tr> ';
-            }
-          ?> 
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo '<tr>';
+    echo '<td>'. $row['FIRST_NAME'].'</td>';
+    echo '<td>'. $row['LAST_NAME'].'</td>';
+    echo '<td>'. $row['JOB_TITLE'].'</td>';
+
+    echo '<td align="right">
+            <div class="btn-group">
+              <a type="button" class="btn btn-primary bg-gradient-primary" href="emp_searchfrm.php?action=edit & id='.$row['EMPLOYEE_ID'] . '">
+                <i class="fas fa-fw fa-list-alt"></i> Détails
+              </a>
+              <div class="btn-group">
+                <a type="button" class="btn btn-primary bg-gradient-primary dropdown no-arrow" data-toggle="dropdown" style="color:white;">
+                  ... <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu text-center" role="menu">
+                  <li>
+                    <a type="button" class="btn btn-warning bg-gradient-warning btn-block" style="border-radius: 0px;" href="emp_edit.php?action=edit & id='.$row['EMPLOYEE_ID']. '">
+                      <i class="fas fa-fw fa-edit"></i> Modifier
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div> 
+          </td>';
+    echo '</tr>';
+  }
+?> 
         </tbody>
       </table>
     </div>
   </div>
 </div>
+
+<?php
+include'../includes/footer.php';
+?>
--- pages/supplier.php
+++ pages/supplier.php
@@ -1,96 +1,115 @@
 <?php
 include'../includes/connection.php';
-
 include'../includes/sidebar.php';
-  $query = 'SELECT ID, t.TYPE
-            FROM users u
-            JOIN type t ON t.TYPE_ID=u.TYPE_ID WHERE ID = '.$_SESSION['MEMBER_ID'].''; 
-  $result = mysqli_query($db, $query) or die (mysqli_error($db));
-  
-  while ($row = mysqli_fetch_assoc($result)) {
-            $Aa = $row['TYPE'];
-                   	
+
+$query = 'SELECT ID, t.TYPE
+          FROM users u
+          JOIN type t ON t.TYPE_ID=u.TYPE_ID WHERE ID = '.$_SESSION['MEMBER_ID'].'';
+$result = mysqli_query($db, $query) or die (mysqli_error($db));
+
+while ($row = mysqli_fetch_assoc($result)) {
+  $Aa = $row['TYPE'];
+
   if ($Aa=='User'){
 ?>
   <script type="text/javascript">
-    //ensuite, l'utilisateur sera redirigé
-    alert("Page restreinte ! Vous serez redirigé vers le POS");
+    // ensuite, rediriger l'utilisateur
+    alert("Page restreinte ! Vous allez être redirigé vers le POS");
     window.location = "pos.php";
   </script>
 <?php
   }           
-}
+} 
 ?>
-          <center><div class="card shadow mb-4 col-xs-12 col-md-8 border-bottom-primary">
-            <div class="card-header py-3">
-              <h4 class="m-2 font-weight-bold text-primary">Détails du fournisseur</h4>
-            </div>
-            <a href="supplier.php?action=add" type="button" class="btn btn-primary bg-gradient-primary">Retour</a>
-            <div class="card-body">
-          <?php 
-            $query = 'SELECT SUPPLIER_ID, COMPANY_NAME, l.PROVINCE, l.CITY, PHONE_NUMBER FROM supplier e join location l on e.LOCATION_ID = l.LOCATION_ID WHERE e.SUPPLIER_ID ='.$_GET['id'];
-            $result = mysqli_query($db, $query) or die(mysqli_error($db));
-              while($row = mysqli_fetch_array($result))
-              {   
-                $zz= $row['SUPPLIER_ID'];
-                $a= $row['COMPANY_NAME'];
-                $b=$row['PROVINCE'];
-                $c=$row['CITY'];
-                $d=$row['PHONE_NUMBER'];
-              }
-              $id = $_GET['id'];
-          ?>
-                
-                    <div class="form-group row text-left">
-                      <div class="col-sm-3 text-primary">
-                        <h5>
-                          Nom de l'entreprise<br>
-                        </h5>
-                      </div>
-                      <div class="col-sm-9">
-                        <h5>
-                          : <?php echo $a; ?><br>
-                        </h5>
-                      </div>
-                    </div>
-                    <div class="form-group row text-left">
-                      <div class="col-sm-3 text-primary">
-                        <h5>
-                          Province<br>
-                        </h5>
-                      </div>
-                      <div class="col-sm-9">
-                        <h5>
-                          : <?php echo $b; ?> <br>
-                        </h5>
-                      </div>
-                    </div>
-                    <div class="form-group row text-left">
-                      <div class="col-sm-3 text-primary">
-                        <h5>
-                          Ville<br>
-                        </h5>
-                      </div>
-                      <div class="col-sm-9">
-                        <h5>
-                          : <?php echo $c; ?> <br>
-                        </h5>
-                      </div>
-                    </div>
-                    <div class="form-group row text-left">
-                      <div class="col-sm-3 text-primary">
-                        <h5>
-                          Numéro de téléphone<br>
-                        </h5>
-                      </div>
-                      <div class="col-sm-9">
-                        <h5>
-                          : <?php echo $d; ?> <br>
-                        </h5>
-                      </div>
-                    </div>
+
+<div class="card shadow mb-4">
+  <div class="card-header py-3">
+    <h4 class="m-2 font-weight-bold text-primary">Fournisseur&nbsp;
+      <a href="#" data-toggle="modal" data-target="#supplierModal" type="button" class="btn btn-primary bg-gradient-primary" style="border-radius: 0px;">
+        <i class="fas fa-fw fa-plus"></i>
+      </a>
+    </h4>
+  </div>
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0"> 
+        <thead>
+          <tr>
+            <th>Nom de l'entreprise</th>
+            <th>Province</th>
+            <th>Ville</th>
+            <th>Numéro de téléphone</th>
+            <th>Option</th>
+          </tr>
+        </thead>
+        <tbody>
+<?php                  
+  $query = 'SELECT SUPPLIER_ID, COMPANY_NAME, l.PROVINCE, l.CITY, PHONE_NUMBER FROM supplier s join location l on s.LOCATION_ID=l.LOCATION_ID';
+  $result = mysqli_query($db, $query) or die (mysqli_error($db));
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo '<tr>';
+    echo '<td>'. $row['COMPANY_NAME'].'</td>';
+    echo '<td>'. $row['PROVINCE'].'</td>';
+    echo '<td>'. $row['CITY'].'</td>';
+    echo '<td>'. $row['PHONE_NUMBER'].'</td>';
+    echo '<td align="right"> <div class="btn-group">
+            <a type="button" class="btn btn-primary bg-gradient-primary" href="sup_searchfrm.php?action=edit & id='.$row['SUPPLIER_ID'] . '"><i class="fas fa-fw fa-list-alt"></i> Détails</a>
+          <div class="btn-group">
+            <a type="button" class="btn btn-primary bg-gradient-primary dropdown no-arrow" data-toggle="dropdown" style="color:white;">
+            ... <span class="caret"></span></a>
+          <ul class="dropdown-menu text-center" role="menu">
+              <li>
+                <a type="button" class="btn btn-warning bg-gradient-warning btn-block" style="border-radius: 0px;" href="sup_edit.php?action=edit & id='.$row['SUPPLIER_ID']. '">
+                  <i class="fas fa-fw fa-edit"></i> Modifier
+                </a>
+              </li> 
+          </ul>
           </div>
+        </div> </td>';
+    echo '</tr> ';
+  }
+?> 
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<!-- Fenêtre modale Fournisseur -->
+<div class="modal fade" id="supplierModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ajouter un fournisseur</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Fermer">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form role="form" method="post" action="sup_transac.php?action=add">
+          
+          <div class="form-group">
+            <input class="form-control" placeholder="Nom de l'entreprise" name="companyname" required>
           </div>
+          <div class="form-group">
+            <select class="form-control" id="province" placeholder="Province" name="province" required></select>
+          </div>
+          <div class="form-group">
+            <select class="form-control" id="city" placeholder="Ville" name="city" required></select>
+          </div>
+          <div class="form-group">
+            <input class="form-control" placeholder="Numéro de téléphone" name="phonenumber" required>
+          </div>
+          <hr>
+          <button type="submit" class="btn btn-success"><i class="fa fa-check fa-fw"></i>Enregistrer</button>
+          <button type="reset" class="btn btn-danger"><i class="fa fa-times fa-fw"></i>Réinitialiser</button>
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>      
+        </form>  
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php
 include'../includes/footer.php';
